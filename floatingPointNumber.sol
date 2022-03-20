909863f4c7fdabc5170da4e3672b2c67e4d5eaac
0xYuan <96657096+ultrasound7@users.noreply.github.com>
2022-03-20 18:39:37 +0800

    Delete floatingPointNumber.sol

--- floatingPointNumber.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.1;
-
-contract floatingPointNumber{
-
-    uint constant floatScale = 1e18;
-    uint constant doubleScale = 1e36;
-    uint constant one = floatScale;
-    uint constant onePercent = 1e16;
-}
